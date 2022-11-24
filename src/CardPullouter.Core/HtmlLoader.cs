02af96495a22392d717ccf26760345d0f2d8ed76
captystacy <captystacy@gmail.com>
2022-11-24 13:15:13 +0300

    add html loader

---
+++ src/CardPullouter.Core/HtmlLoader.cs
@@ -0,0 +1,37 @@
+﻿using Calabonga.OperationResults;
+using CardPullouter.Core.Interfaces;
+using PuppeteerSharp;
+
+namespace CardPullouter.Core
+{
+    public class HtmlLoader : IHtmlLoader
+    {
+        public async Task<OperationResult<string>> LoadAndWaitForSelector(string uri, string selector)
+        {
+            var operation = OperationResult.CreateResult<string>();
+
+            IBrowser browser;
+            try
+            {
+                browser = await Puppeteer.LaunchAsync(new LaunchOptions
+                {
+                    Headless = true,
+                    ExecutablePath = @"c:\Program Files\Google\Chrome\Application\chrome.exe"
+                });
+            }
+            catch (Exception exception)
+            {
+                operation.AddError("Something went wrong when launching headless browser", exception);
+                return operation;
+            }
+
+            await using var page = await browser.NewPageAsync();
+            await page.GoToAsync(uri);
+            await page.WaitForSelectorAsync(selector);
+
+            operation.Result = await page.GetContentAsync();
+
+            return operation;
+        }
+    }
+}
---
+++ src/CardPullouter.Core/Interfaces/IHtmlLoader.cs
@@ -0,0 +1,8 @@
+﻿using Calabonga.OperationResults;
+
+namespace CardPullouter.Core.Interfaces;
+
+public interface IHtmlLoader
+{
+    Task<OperationResult<string>> LoadAndWaitForSelector(string uri, string selector);
+}
